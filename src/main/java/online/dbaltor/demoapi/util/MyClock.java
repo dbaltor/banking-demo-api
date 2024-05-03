b60d73d1a6785a1eb97bf6de2de9ddad7406f6df
dbaltor <39310315+dbaltor@users.noreply.github.com>
2024-05-03 18:33:11 +0100

    add code coverage report

--- src/main/java/online/dbaltor/demoapi/util/MyClock.java
+++ src/main/java/online/dbaltor/demoapi/util/MyClock.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+@Generated
 @Component
 public class MyClock {
     private static final DateTimeFormatter DD_MM_YYYY = DateTimeFormatter.ofPattern("dd/MM/yyyy");
