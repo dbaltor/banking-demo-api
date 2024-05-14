0df972862f84e64cb09fe429d098d8419eca817c
dbaltor <39310315+dbaltor@users.noreply.github.com>
2024-05-14 16:48:38 +0100

    reformat file

--- src/test/java/online/dbaltor/demoapi/acceptance/steps/CucumberSpringContextConfig.java
+++ src/test/java/online/dbaltor/demoapi/acceptance/steps/CucumberSpringContextConfig.java
@@ -1,22 +1,20 @@
 package online.dbaltor.demoapi.acceptance.steps;
 
+import io.cucumber.spring.CucumberContextConfiguration;
 import online.dbaltor.demoapi.util.MyClock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import io.cucumber.spring.CucumberContextConfiguration;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class CucumberSpringContextConfig {
-    @MockBean
-    MyClock clock;
+    @MockBean MyClock clock;
 
-    @LocalServerPort
-    protected int port;
+    @LocalServerPort protected int port;
+
     protected int getServerPort() {
         return port;
     }
 }
-
--- src/test/java/online/dbaltor/demoapi/acceptance/steps/GetBankStatementStepDefinitions.java
+++ src/test/java/online/dbaltor/demoapi/acceptance/steps/GetBankStatementStepDefinitions.java
@@ -1,18 +1,17 @@
 package online.dbaltor.demoapi.acceptance.steps;
 
-import io.cucumber.datatable.DataTable;
-import io.cucumber.java8.En;
-import lombok.val;
-import online.dbaltor.demoapi.util.MyClock;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import static java.util.stream.Collectors.joining;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.BDDMockito.given;
 
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java8.En;
 import java.util.List;
 import java.util.Map;
+import lombok.val;
+import online.dbaltor.demoapi.util.MyClock;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class GetBankStatementStepDefinitions extends CucumberSpringContextConfig implements En {
     private static final String STATEMENT_HEADER = "DATE       | AMOUNT | BALANCE\n";
@@ -26,27 +25,39 @@ public class GetBankStatementStepDefinitions extends CucumberSpringContextConfig
 
         Given("Jane has a bank account number {word}", (String number) -> accountNumber = number);
 
-        And("she deposits {word} GBP on {word}", (String amount, String date) -> {
-            given(clock.todayAsString()).willReturn(date);
-            bankingClient.deposit(accountNumber, amount);
-        });
+        And(
+                "she deposits {word} GBP on {word}",
+                (String amount, String date) -> {
+                    given(clock.todayAsString()).willReturn(date);
+                    bankingClient.deposit(accountNumber, amount);
+                });
 
-        And("she withdraws {word} GBP on {word}", (String amount, String date) -> {
-            given(clock.todayAsString()).willReturn(date);
-            bankingClient.withdraw(accountNumber, amount);
-        }) ;
+        And(
+                "she withdraws {word} GBP on {word}",
+                (String amount, String date) -> {
+                    given(clock.todayAsString()).willReturn(date);
+                    bankingClient.withdraw(accountNumber, amount);
+                });
 
-        When("she gets her bank statement", () -> statement = bankingClient.getStatement(accountNumber));
+        When(
+                "she gets her bank statement",
+                () -> statement = bankingClient.getStatement(accountNumber));
 
-        Then("she should be shown the following:", (DataTable dataTable ) -> {
-            val expectedStatement = STATEMENT_HEADER + statementLines(dataTable.asMaps());
-            assertThat(statement, is(expectedStatement));
-        });
+        Then(
+                "she should be shown the following:",
+                (DataTable dataTable) -> {
+                    val expectedStatement = STATEMENT_HEADER + statementLines(dataTable.asMaps());
+                    assertThat(statement, is(expectedStatement));
+                });
     }
 
     private static String statementLines(List<Map<String, String>> statementLines) {
         return statementLines.stream()
-            .map(m -> "%s | %s | %s".formatted(m.get("DATE"), m.get("AMOUNT"), m.get("BALANCE")))
-            .collect(joining("\n"));
+                .map(
+                        m ->
+                                "%s | %s | %s"
+                                        .formatted(
+                                                m.get("DATE"), m.get("AMOUNT"), m.get("BALANCE")))
+                .collect(joining("\n"));
     }
 }
